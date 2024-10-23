cb216ea00490c05d989dfbef6db0039f5dba6fd6
More-Narawit <narawit.han@spumail.net>
2024-10-23 12:03:14 +0700

    debug img5

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
             <h1>Introduce yourself</h1>
             <div className='introduce-container'>
                 <div className='img-container'>
+                    <img className='img' src="./public/human.jpg" alt="" />
                 </div>
                 <div className='text-container'>
                     <h2><u>I am More</u></h2>
